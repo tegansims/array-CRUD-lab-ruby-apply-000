4986187f37562d3660659e984d04a8482d731e07
Tegan Sims <tegansims@hotmail.com>
2019-05-16 20:39:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,19 +11,19 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  element.unshift
 end
 
 def remove_element_from_end_of_array(array)
-  
+  element.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  element.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  puts array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
